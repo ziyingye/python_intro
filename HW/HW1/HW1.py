9511916f6715141368d51125e60b5fdc74a339de
yifanwu2828 <yiw084@ucsd.edu>
2021-04-25 19:57:48 -0400

    TODO: HW1

---
+++ HW/HW1/HW1.py
@@ -0,0 +1,39 @@
+# import required packages
+#     deque
+
+
+if __name__ == '__main__':
+    pass
+
+    ''' 1.Summation: '''
+    # TODO:
+    # Write a program to perform summation over `lst` and print the results and store in variable ans
+    lst1 = list(range(0, 10, 2))
+    # ...
+    # ans1 =
+
+    ''' 2. Indexing and Sorting '''
+    # TODO:
+    # Write a program to sorting the list in descending order
+    # and inset a list of string in begin, middle and end
+    # use both list and deque to solve the problem
+    lst2 = [1, 50, 21, -6, 128, 10]
+    str_lst = ["Hello", "python", "world"]
+    # ...
+    # ans2 =
+
+    ''' 3. Even Numbers '''
+    # TODO:
+    # Given a tuple of integers, extract the even elements and sum them up
+    # Hint 1: Iterate through tuple use for loop
+    # Hint 2: Check even number by `%2 == 0`
+    # Hint 3: Either use variable addition in each iteration ,
+    #         or store even value first and perfom summation same as in Part 1
+    # ...
+    # ans3 =
+
+    ''' 4. Create an Immutable Tuple of Element from List'''
+    # Hint: extract every element and convert nested list(list of list or ...) in to a single list
+    #       ans should be a single tuple contain all element in lst 4 and ensure nothing is mutable
+    lst4 = ["Hi", [1, 2, 3], 1.5, 'a', ["Today", "2", 30]]
+    # ans4 =
