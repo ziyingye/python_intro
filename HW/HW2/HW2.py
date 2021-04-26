fab6b8d0d12763381c5ecf808737577eecda1b22
yifanwu2828 <yiw084@ucsd.edu>
2021-04-26 16:55:41 -0400

    Create HW2

---
+++ HW/HW2/HW2.py
@@ -0,0 +1,33 @@
+
+
+
+if __name__ == '__main__':
+    pass
+
+    # Given a sequence of numbers, find the all odd number and between [-5, 5]
+    # Using a while loop
+    # Using a for loop
+    # Using list comprehension
+    # Given a sequence of numbers, find the all odd number and between [-5, 5]
+    # Using a while loop
+    # Using a for loop
+    # Using list comprehension
+    # Hint1: In part 1 you should initiate i as index of seq and increment it at the end of each loop iteration
+    #       extract the element by call seq[INDEX]
+
+    # Hint2: In part 2 you can use the same strategy as 1 but a better way is to directly extract the value
+    #       # e.g. for i in seq: ... Note here i is not the index of seq, instead it is the value of seq at that index
+
+    # Hint3: IN part 3 if you implementation is same as described as in part2 you can simply convert it into
+    #       List comprehension as we shown in Lecture 2
+
+    # End Notes: Always check back on Lecture covered. Google is your Friend!!
+    seq = list(range(-20, 20, 3))
+    print(seq)
+
+    '''1. While Loop'''
+    # ...
+    '''2. For Loop'''
+    # ...
+    '''3. List Comprehension'''
+    # ...
