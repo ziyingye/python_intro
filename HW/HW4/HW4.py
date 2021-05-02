2514f640912a7fe1b5ee2b20a35f1a6b6257e1b9
yifanwu2828 <yiw084@ucsd.edu>
2021-05-02 17:53:46 -0400

    add

---
+++ HW/HW4/HW4.py
@@ -0,0 +1,26 @@
+
+
+if __name__ == '__main__':
+    pass
+
+    '''
+    P1. Write a Python program to reverse a string
+    '''
+    # Sample String : "1234abcd"
+
+    '''
+    P2. Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters
+    '''
+    # Sample String : 'The BIGGEST Elephant'
+
+
+    '''
+    P3. Write a Python function that takes a list and returns a new list with unique elements of the first list
+    '''
+    # Sample List : [1,2,3,3,3,3,4,5]
+
+
+    '''
+    P4. Write a Python program to insert an element at the beginning of a given OrderedDictionary
+    '''
+    # Original OrderedDict: OrderedDict([('color1', 'Red'), ('color2', 'Green'), ('color3', 'Blue')])
